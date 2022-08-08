fb078e57a65e7df91b9381f311daf888254182b1
Quyen Luu <luqu0501@gmail.com>
2022-08-08 11:38:59 +0700

    🎉: init project struct

---
+++ webpack.dev.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/client/index.js',
+  mode: 'development',
+  devtool: 'source-map',
+  stats: 'verbose',
+  output: {
+    library: 'app',
+    libraryTarget: 'var',
+  },
+  module: {
+    rules: [
+      {
+        test: '/.js$/',
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/client/public/index.html',
+      filename: './index.html',
+    }),
+    new CleanWebpackPlugin({
+      dry: true,
+      verbose: true,
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false,
+    }),
+  ],
+  devServer: {
+    port: 3001,
+  },
+};
---
+++ webpack.prod.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/client/index.js',
+  mode: 'production',
+  devtool: 'source-map',
+  stats: 'verbose',
+  output: {
+    library: 'app',
+    libraryTarget: 'var',
+  },
+  module: {
+    rules: [
+      {
+        test: '/.js$/',
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]',
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/client/public/index.html',
+      filename: './index.html',
+    }),
+    new CleanWebpackPlugin({
+      dry: true,
+      verbose: true,
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false,
+    }),
+    new WorkboxPlugin.GenerateSW(),
+  ],
+};
